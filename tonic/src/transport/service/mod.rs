a77a8a7bee98c74a797a7f7335bb9e16b65b654d
boxdot <d@zerovolt.org>
2020-11-12 09:47:27 +0100

    Quick and dirty patch to compile client-only transport on wasm32.

--- tonic/src/transport/service/mod.rs
+++ tonic/src/transport/service/mod.rs
@@ -1,9 +1,11 @@
 mod add_origin;
 mod connection;
 mod connector;
+#[cfg(feature = "transport")]
 mod discover;
 mod io;
 mod reconnect;
+#[cfg(feature = "transport")]
 mod router;
 #[cfg(feature = "tls")]
 mod tls;
@@ -12,8 +14,11 @@ mod user_agent;
 pub(crate) use self::add_origin::AddOrigin;
 pub(crate) use self::connection::Connection;
 pub(crate) use self::connector::connector;
+#[cfg(feature = "transport")]
 pub(crate) use self::discover::DynamicServiceStream;
+#[cfg(feature = "transport")]
 pub(crate) use self::io::ServerIo;
+#[cfg(feature = "transport")]
 pub(crate) use self::router::{Or, Routes};
 #[cfg(feature = "tls")]
 pub(crate) use self::tls::{TlsAcceptor, TlsConnector};
